e4791f8604188a4bace2a081b75b9f43d8f307ad
avgusev <a.v.gusev79@yandex.ru>
2024-04-18 02:11:03 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,104 +1,27 @@
 <template>
-  <Navbar/>
-  <div class="con">
-    <router-view/>
+  <div class="App">
+    <div class="epgContainer">
+      <ChannelsPanel />
+      <MainContainer />
+    </div>
   </div>
-  <Footer/>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar.vue"
-import Footer from "@/components/Footer.vue"
+import TopBar from './components/topbar/topbar';
+import ChannelsPanel from './components/channelspanel/channelspanel';
+import MainContainer from './components/maincontainer/maincontainer';
+import { ChannelsProvider } from './contexts/channelscontext';
+
 export default {
   components: {
-    Navbar,
-    Footer
-  }
-}
+    TopBar,
+    ChannelsPanel,
+    MainContainer
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-}
-
-ul[class],
-ol[class] {
-  padding: 0;
-}
-
-body,
-h1,
-h2,
-h3,
-h4,
-p,
-ul[class],
-ol[class],
-li,
-figure,
-figcaption,
-blockquote,
-dl,
-dd {
-  margin: 0;
-}
-
-body {
-  min-height: 100vh;
-  scroll-behavior: smooth;
-  text-rendering: optimizeSpeed;
-  line-height: 1.5;
-}
-
-ul[class],
-ol[class] {
-  list-style: none;
-}
-
-a:not([class]) {
-  text-decoration-skip-ink: auto;
-}
-
-
-img {
-  max-width: 100%;
-  display: block;
-}
-
-
-article > * + * {
-  margin-top: 1em;
-}
-
-input,
-button,
-textarea,
-select {
-  font: inherit;
-}
-
-
-@media (prefers-reduced-motion: reduce) {
-  * {
-    animation-duration: 0.01ms !important;
-    animation-iteration-count: 1 !important;
-    transition-duration: 0.01ms !important;
-    scroll-behavior: auto !important;
-  }
-}
-
-.con {
-  margin-left: 2rem;
-  margin-right: 2rem;
-}
+<style scoped lang="scss">
+@import './App.scss';
 </style>
