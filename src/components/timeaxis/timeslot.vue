658502a09b261cde7f56231de29341e321784f40
avgusev <a.v.gusev79@yandex.ru>
2024-04-18 02:20:54 +0300

    first commit

--- src/components/timeaxis/timeslot.vue
+++ src/components/timeaxis/timeslot.vue
@@ -1,22 +1,40 @@
-import { TIMESLOTWIDTH } from "../../settings/defaults"
+<template>
+  <div class="timeSlotContainer" :style="{ width: TIMESLOTWIDTH + 'px' }">
+    <div class="timeSlot">
+      <span class="label">{{ label }}</span>
+    </div>
+  </div>
+</template>
 
-// export component
+<script>
+import { TIMESLOTWIDTH } from '../../settings/defaults';
+
 export default {
-  name: "time-slot",
-  functional: true,
-  render(h, c) {
-    const props = c.props
-    return (
-      <div
-        class="timeSlotContainer"
-        style={{
-          width: TIMESLOTWIDTH
-        }}
-      >
-        <div class="timeSlot">
-          <span class="label">{props.label}</span>
-        </div>
-      </div>
-    )
-  }
+  name: 'TimeSlot',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      TIMESLOTWIDTH,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.timeSlotContainer {
+  /* Add any necessary styles */
 }
+
+.timeSlot {
+  /* Add any necessary styles */
+}
+
+.label {
+  /* Add any necessary styles */
+}
+</style>
